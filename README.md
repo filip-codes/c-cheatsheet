9487d71301109138ffe9f4d8ec1f06b0b378e259
filip-codes <filip@bitecorn.com>
2023-06-04 13:00:49 +0200

    added cmake-simple example

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # c-cheatsheet
+
+This is a collection of C snippets and examples.
+
+## Examples
+- [cmake-simple](examples/cmake-simple)
---
+++ examples/cmake-simple/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @brief This is a simple example of a C program.
+ *
+ * @return int exit_code
+ */
+int main() {
+    // Print a message to the console.
+    puts("Hello World!");
+
+    // Return 0 to indicate success.
+    return 0;
+}
